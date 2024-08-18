c0d9706557517597dced00069e7809a9ce4e649e
yuli <yuli@YULIST-MCD.local>
2024-08-18 14:57:07 +0300

    clean terraform files

---
+++ clean-tf.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Find directories containing *.tf files
+directories=$(find . -type f -name "*.tf" -exec dirname {} \; | sort -u)
+
+# Loop through each directory and run terraform fmt
+for dir in $directories; do
+  echo "Running 'terraform fmt' in $dir"
+  (cd "$dir" && terraform fmt)
+done
