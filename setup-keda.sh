6b44f76fc8031befc2f07867ab1e9afd2eab0b7a
yuli <yuli@YULIST-MCD.local>
2024-09-24 09:39:17 +0300

    keda setup script

---
+++ setup-keda.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+CLUSTER_NAME=$(terraform output -raw cluster_name)
+if [[ -z "$CLUSTER_NAME" ]]; then
+    echo "Failed to get CLUSTER_NAME from terraform output"
+    exit
+fi
+
+RESOURCE_GROUP_NAME=$(terraform output -raw resource_group_name)
+if [[ -z "$RESOURCE_GROUP_NAME" ]]; then
+    echo "Failed to get RESOURCE_GROUP_NAME from terraform output"
+    exit
+fi
+
+echo "Sync kubernetes configuration"
+echo "Cluster name: $CLUSTER_NAME"
+
+az aks get-credentials --resource-group $RESOURCE_GROUP_NAME --name $CLUSTER_NAME
+kubectl config set-context $CLUSTER_NAME
+
+az aks update --resource-group $RESOURCE_GROUP_NAME --name $CLUSTER_NAME --enable-keda
